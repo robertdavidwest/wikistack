3ce592b83bcb6b159f0cad968f119bbc912ba65d
Robert West <robert.david.west@gmail.com>
2022-11-17 13:00:22 -0500

    Added a Page model to index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,13 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/wikistack');
+const Sequelize = require("sequelize");
+const db = new Sequelize("postgres://localhost:5432/wikistack");
 
 module.exports = {
-    db
-}
+  db,
+};
+
+const Page = db.define("pages", {
+  title: { type: Sequelize.STRING },
+  slug: { type: Sequelize.STRING },
+  content: { type: Sequelize.TEXT },
+  status: { type: Sequelize.BOOLEAN },
+});
