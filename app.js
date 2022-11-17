dd69b1680e12bf5e8c59e914d2bb44373101bc51
Robert West <robert.david.west@gmail.com>
2022-11-17 15:46:23 -0500

    Added content to addPage template , started building /wiki  routes

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-const express = require('express')
-const morgan = require('morgan')
-const wikiRouter = require(`./routes/wiki`)
-const userRouter = require(`./routes/users`)
-const { db, Page, User } = require('./models')
+const express = require("express");
+const morgan = require("morgan");
+const wikiRouter = require(`./routes/wiki`);
+const userRouter = require(`./routes/users`);
+const { db, Page, User } = require("./models");
 const app = express();
-const layout = require('./views/layout')
+const layout = require("./views/layout");
 
-app.use(morgan('dev'))
-app.use(express.urlencoded({extended: false}))
-app.use(express.static(__dirname + '/public'))
-app.use('/wiki', wikiRouter)
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/public"));
+app.use("/wiki", wikiRouter);
 // app.use('/users', usersRouter)
 
-app.get('/', (req, res, next) => {
-    res.send(layout(''));
-})
+app.get("/", (req, res, next) => {
+  res.redirect("/wiki");
+});
 
 const port = 1337;
 
-const init = async() => {
-    await db.sync({force: true})
-    
-    app.listen(port, () => {
-      console.log("App listening");
-    });
+const init = async () => {
+  await db.sync({ force: true });
+
+  app.listen(port, () => {
+    console.log("App listening");
+  });
 };
 
-init()
+init();
 
-db.authenticate() 
-  .then(() => { 
-    console.log('connected to the database'); 
-})
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,15 +1,34 @@
-let express = require('express')
+const { Page } = require("../models");
+let express = require("express");
 let wikiRouter = express.Router();
-let { addPage } = require('../views')
+let { addPage } = require("../views");
+let { wikiPage } = require("../views");
 
-wikiRouter.get('/', (req, res, next) => {
-    // res.redirect('/wiki')
-})
-// wikiRouter.post('/', (req, res) => {
-//     res.send(
-// })
-wikiRouter.get('/add', (req, res) => {
-    res.send(addPage())
-})
+wikiRouter.get("/", (req, res, next) => {
+  res.send("got to GET /wiki/");
+});
 
-module.exports = wikiRouter
+function generateSlug(title) {
+  // Removes all non-alphanumeric characters from title
+  // And make whitespace underscore
+  return title.replace(/\s+/g, "_").replace(/\W/g, "");
+}
+
+wikiRouter.post("/", async (req, res, next) => {
+  console.log(req.body);
+  const title = req.body.title;
+  const slug = generateSlug(title);
+  const content = req.body.content;
+  try {
+    const page = await Page.create({ title, slug, content });
+  } catch (error) {
+    next(error);
+  }
+  res.redirect("/");
+});
+
+wikiRouter.get("/add", (req, res) => {
+  res.send(addPage());
+});
+
+module.exports = wikiRouter;
