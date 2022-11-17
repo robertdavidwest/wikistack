bb149184c06b0d41255880dcac39b54b694a466f
Steven King <king.steven.t@gmail.com>
2022-11-17 10:46:10 -0700

    Steven done driving

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const morgan = require('morgan')
+const { db } = require('./models')
 const app = express();
+const layout = require('./views/layout')
 
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res, next) => {
-    res.send('Hello World')
+    res.send(layout(''));
 })
 
 const port = 1337;
 app.listen(port, () => {
     console.log("App listening")
+})
+
+db.authenticate() 
+  .then(() => { 
+    console.log('connected to the database'); 
 })
